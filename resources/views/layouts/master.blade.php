402780a70413a96e00c327982229b357a383b69a
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-01-09 09:01:29 +0100

    Update 3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-
-  <title>The HTML5 Herald</title>
+  <meta name="viewport">
+  <title>Document</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
 
